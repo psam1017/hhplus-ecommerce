0128882d6a87e4624eed652bcd0ac3fabe9a11bc
sam_nodeblack <psam1017@nodeblack.io>
2024-10-16 00:36:15 +0900

    Feat: OrderDataPlatform 에 DIP 적용

--- src/main/java/hhplus/ecommerce/server/infrastructure/data/OrderDataPlatform.java
+++ src/main/java/hhplus/ecommerce/server/infrastructure/data/OrderDataPlatform.java
@@ -1,13 +1,8 @@
 package hhplus.ecommerce.server.infrastructure.data;
 
-import org.springframework.stereotype.Component;
-
 import java.util.Map;
 
-@Component
-public class OrderDataPlatform {
+public interface OrderDataPlatform {
 
-    public void saveOrderData(Map<Long, Integer> itemIdItemAmountMap) {
-        // TODO: 2024-10-15 implement this method
-    }
+    void saveOrderData(Map<Long, Integer> itemIdItemAmountMap);
 }
