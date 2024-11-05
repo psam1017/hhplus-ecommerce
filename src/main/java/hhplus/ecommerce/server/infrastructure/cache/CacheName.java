51aad534eaf96b45592f3a3cd35a8977e7a5d5d3
sam_nodeblack <psam1017@nodeblack.io>
2024-11-05 19:30:20 +0900

    Feat: 캐시 설정 및 적용

---
+++ src/main/java/hhplus/ecommerce/server/infrastructure/cache/CacheName.java
@@ -0,0 +1,8 @@
+package hhplus.ecommerce.server.infrastructure.cache;
+
+public final class CacheName {
+
+    public static final String ITEMS_PAGE = "cache:items:page";
+    public static final String ITEMS_TOP = "cache:items:top";
+    public static final String ITEM_WARM = "cache:item:warm";
+}
